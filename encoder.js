fcf04e9ce0e8cc928890dc6fd36b090d4daaa7ae
Densel Martin <denselmartin@gmail.com>
2016-10-18 11:15:37 -0700

    echo
    got it to echo

--- encoder.js
+++ encoder.js
@@ -1,4 +1,25 @@
-$("#translationArea").keydown(function(event){
-  if (event.keyCode == 13);
-  
+$("#inputText").keyup(function(e) {
+ var inputText = document.getElementById("inputText").value;
+ document.getElementById("textArea").innerHTML = inputText;
+
+ if ($("input:checked").val()== "echo") {
+document.getElementById("textArea").innerHTML = inputText;
+
+} else if ($("input:checked").val()== "caesar") {
+document.getElementById("textArea").innerHTML = inputText;
+
+} else if ($("input:checked").val()== "heiroglyphic") {
+document.getElementById("textArea").innerHTML = inputText;
+
+}
+
 });
+
+$("input[type='radio']").click(function() {
+$("input:checked").prop('checked', false );
+$(this).prop('checked', true )
+});
+
+
+
+//radio button check part 4
